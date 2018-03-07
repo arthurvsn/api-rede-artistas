9f67e9720d7fddee836d969baacc79c8119e64f6
arthurvsn <arthurvsn@gmail.com>
2018-03-07 19:01:39 -0300

    Inicio das autenticacoes

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -7,6 +7,11 @@ use \App\Usuario;
 
 class UsuariosController extends Controller
 {
+    public function __construct() 
+    {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
+    
     public function index()
     {
         $usuarios = Usuario::get();
